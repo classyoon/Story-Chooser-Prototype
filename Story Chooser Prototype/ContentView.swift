6b16eb7408135171c05ed25ec4da4d0427c584f6
Conner Yoon <con.yoon@icloud.com>
2022-09-01 10:56:48 -0400

    Initial Commit

---
+++ Story Chooser Prototype/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Story Chooser Prototype
+//
+//  Created by Conner Yoon on 9/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Story Chooser Prototype/Story_Chooser_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Story_Chooser_PrototypeApp.swift
+//  Story Chooser Prototype
+//
+//  Created by Conner Yoon on 9/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Story_Chooser_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
